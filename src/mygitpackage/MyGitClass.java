500fdbcedee7b54995b2b211ace3388ee3635dbd
yakup <ynoktayorulmaz@gmail.com>
2021-04-03 19:50:53 +0300

    versiyon 03

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,6 +5,7 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
 		System.out.println("my second commit");
+		System.out.println("my third commit");
 
 	}
 

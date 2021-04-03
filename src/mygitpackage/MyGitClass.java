87f49a292649accacbb46550d6142d69ab0b64d0
yakup <ynoktayorulmaz@gmail.com>
2021-04-03 19:44:03 +0300

    version 02

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -3,7 +3,8 @@ package mygitpackage;
 public class MyGitClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Version 1");
+		System.out.println("my second commit");
 
 	}
 
